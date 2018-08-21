a96e13163d96216ed7318860decdce94a9cc18a7
TheUnlocked <10186337+TheUnlocked@users.noreply.github.com>
2018-08-20 19:05:22 -0700

    Added more information to eigen

--- eigen/rendermath.js
+++ eigen/rendermath.js
@@ -3,25 +3,28 @@ function renderMath(){
         let values = getEigenValues();
         let vectors = getNormalizedEigenVectors();
 
-        let start = "\\LARGE{";
+        let start = "\\large{";
         let cap = "}";
 
         let displayString = "";
 
         if (vectors.length == 2){
             displayString += `
-            v_{\\lambda=${roundToPlaces(values[0], 2)}}= \\langle ${roundToPlaces(vectors[0][0], 2)}t, ${roundToPlaces(vectors[0][1], 2)}t \\rangle \\;\\;
+            v_{\\lambda=${roundToPlaces(values[0], 2)}}= \\langle ${roundToPlaces(vectors[0][0], 2)}t, ${roundToPlaces(vectors[0][1], 2)}t \\rangle ; \\;\\;
             v_{\\lambda=${roundToPlaces(values[1], 2)}}= \\langle ${roundToPlaces(vectors[1][0], 2)}s, ${roundToPlaces(vectors[1][1], 2)}s \\rangle`;
 
             if (targetedPoint){
                 let coefficients = getVectorCoefficients(vectors, targetedPoint);
 
-                displayString += `}\\\\
-                    \\LARGE{
+                displayString += `}\\\\ \\large{
                     v_{selected}=${roundToPlaces(coefficients[0], 2)}\\langle ${roundToPlaces(vectors[0][0], 2)}, ${roundToPlaces(vectors[0][1], 2)} \\rangle +
                     ${roundToPlaces(coefficients[1], 2)}\\langle ${roundToPlaces(vectors[1][0], 2)}, ${roundToPlaces(vectors[1][1], 2)} \\rangle =
                     \\langle ${roundToPlaces(targetedPoint[0], 2)}, ${roundToPlaces(targetedPoint[1], 2)} \\rangle`;
             }
+
+            displayString += `}\\\\ \\large{
+                v_{\\lambda=${roundToPlaces(values[0], 2)}_{transformed}}=${roundToPlaces(values[0], 2)}v_{\\lambda=${roundToPlaces(values[0], 2)}_{initial}} ; \\;\\;
+                v_{\\lambda=${roundToPlaces(values[1], 2)}_{transformed}}=${roundToPlaces(values[1], 2)}v_{\\lambda=${roundToPlaces(values[1], 2)}_{initial}}`;
         }
         else if (vectors.length == 1){
             displayString += `v_{\\lambda=${roundToPlaces(values[0], 2)}}= \\langle ${roundToPlaces(vectors[0][0], 2)}t, ${roundToPlaces(vectors[0][1], 2)}t \\rangle`;
