f859d4ce42326c4f2f5ec18383a0db01e799212a
Unlocked <10186337+TheUnlocked@users.noreply.github.com>
2018-08-21 10:16:18 -0700

    Added color-coding and adjusted cyan

--- eigen/rendermath.js
+++ eigen/rendermath.js
@@ -10,24 +10,25 @@ function renderMath(){
 
         if (vectors.length == 2){
             displayString += `
-            v_{\\lambda=${roundToPlaces(values[0], 2)}}= \\langle ${roundToPlaces(vectors[0][0], 2)}t, ${roundToPlaces(vectors[0][1], 2)}t \\rangle ; \\;\\;
-            v_{\\lambda=${roundToPlaces(values[1], 2)}}= \\langle ${roundToPlaces(vectors[1][0], 2)}s, ${roundToPlaces(vectors[1][1], 2)}s \\rangle`;
+            \\textcolor{#FF0000}{ v_{\\lambda=${roundToPlaces(values[0], 2)}} \\textcolor{#000000}{=} \\langle ${roundToPlaces(vectors[0][0], 2)}t, ${roundToPlaces(vectors[0][1], 2)}t \\rangle } ; \\;\\;
+            \\textcolor{#0000FF}{ v_{\\lambda=${roundToPlaces(values[1], 2)}} \\textcolor{#000000}{=} \\langle ${roundToPlaces(vectors[1][0], 2)}s, ${roundToPlaces(vectors[1][1], 2)}s \\rangle }`;
 
             if (targetedPoint){
                 let coefficients = getVectorCoefficients(vectors, targetedPoint);
 
                 displayString += `}\\\\ \\large{
-                    v_{selected}=${roundToPlaces(coefficients[0], 2)}\\langle ${roundToPlaces(vectors[0][0], 2)}, ${roundToPlaces(vectors[0][1], 2)} \\rangle +
-                    ${roundToPlaces(coefficients[1], 2)}\\langle ${roundToPlaces(vectors[1][0], 2)}, ${roundToPlaces(vectors[1][1], 2)} \\rangle =
-                    \\langle ${roundToPlaces(targetedPoint[0], 2)}, ${roundToPlaces(targetedPoint[1], 2)} \\rangle`;
+                    \\textcolor{#FF00FF}{ v_{selected} } =
+                    \\textcolor{#FFC800}{ ${roundToPlaces(coefficients[0], 2)} } \\textcolor{#FF0000}{ \\langle ${roundToPlaces(vectors[0][0], 2)}, ${roundToPlaces(vectors[0][1], 2)} \\rangle } +
+                    \\textcolor{#46C8FF}{ ${roundToPlaces(coefficients[1], 2)} } \\textcolor{#0000FF}{ \\langle ${roundToPlaces(vectors[1][0], 2)}, ${roundToPlaces(vectors[1][1], 2)} \\rangle } =
+                    \\textcolor{#FF00FF}{ \\langle ${roundToPlaces(targetedPoint[0], 2)}, ${roundToPlaces(targetedPoint[1], 2)} \\rangle }`;
             }
 
             displayString += `}\\\\ \\large{
-                v_{\\lambda=${roundToPlaces(values[0], 2)}_{transformed}}=${roundToPlaces(values[0], 2)}v_{\\lambda=${roundToPlaces(values[0], 2)}_{initial}} ; \\;\\;
-                v_{\\lambda=${roundToPlaces(values[1], 2)}_{transformed}}=${roundToPlaces(values[1], 2)}v_{\\lambda=${roundToPlaces(values[1], 2)}_{initial}}`;
+                \\textcolor{#FF0000}{ v_{\\lambda=${roundToPlaces(values[0], 2)}_{transformed}} \\textcolor{#000000}{=} ${roundToPlaces(values[0], 2)}v_{\\lambda=${roundToPlaces(values[0], 2)}_{initial}} } ; \\;\\;
+                \\textcolor{#0000FF}{ v_{\\lambda=${roundToPlaces(values[1], 2)}_{transformed}} \\textcolor{#000000}{=} ${roundToPlaces(values[1], 2)}v_{\\lambda=${roundToPlaces(values[1], 2)}_{initial}} }`;
         }
         else if (vectors.length == 1){
-            displayString += `v_{\\lambda=${roundToPlaces(values[0], 2)}}= \\langle ${roundToPlaces(vectors[0][0], 2)}t, ${roundToPlaces(vectors[0][1], 2)}t \\rangle`;
+            displayString += `\\textcolor{#FF0000}{ v_{\\lambda=${roundToPlaces(values[0], 2)}} \\textcolor{#000000}{=} \\langle ${roundToPlaces(vectors[0][0], 2)}t, ${roundToPlaces(vectors[0][1], 2)}t \\rangle }`;
         }
 
         katex.render(start + displayString + cap, document.getElementById('katexRender'));
