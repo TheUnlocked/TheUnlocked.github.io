36b3c0bac43cb772323b3cb10d315dcd2f6fa97b
Unlocked <10186337+TheUnlocked@users.noreply.github.com>
2019-12-19 17:55:22 -0800

    Auto-add description flags button

--- bettermixer/selfservice/main.js
+++ bettermixer/selfservice/main.js
@@ -2,7 +2,7 @@
 const encodeHTMLEntities = str => str.replace(/[\u00A0-\u9999<>\&\"]/gim, i => '&#'+i.charCodeAt(0)+';')
 const getFlagStringFromData = channelData => `<img alt="!!better-mixer-desc-flags!!${encodeHTMLEntities(JSON.stringify(channelData))}" src="https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png" />`;
 
-const updateChannelCustomizationOutput = () => {
+const getChannelFlags = () => {
     const twitchUsername = document.getElementById('ch-twitch-username').value;
     const ffzSync = document.getElementById('ch-ffz-sync').checked;
     const bttvSync = document.getElementById('ch-bttv-sync').checked;
@@ -24,5 +24,92 @@ const updateChannelCustomizationOutput = () => {
     data.bttv.globals = bttvGlobals;
     data.ffz.globals = ffzGlobals;
 
-    document.getElementById('ch-desc-printout').innerHTML = encodeHTMLEntities(getFlagStringFromData(data));
+    return getFlagStringFromData(data);
+};
+
+const updateChannelCustomizationOutput = () => {
+    document.getElementById('ch-desc-printout').innerHTML = encodeHTMLEntities(getChannelFlags());
+}
+
+const updateMixerDescription = async (existingWindow) => {
+    const authWindow = existingWindow || window.open("", "_blank");
+
+    if (localStorage.getItem('mixerAuthToken')) {
+        try {
+            const currentUserResponse = await fetch('https://mixer.com/api/v1/users/current', {
+                method: "GET",
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('mixerAuthToken')}`
+                }
+            });
+            if (!currentUserResponse.ok) throw currentUserResponse;
+            const currentUser = await currentUserResponse.json();
+            const oldDesc = currentUser.channel.description;
+            const oldFlags = /<img.*?!!better-mixer-desc-flags!!.*?>/g.exec(oldDesc)[0];
+            const oldFlagsIndex = oldDesc.indexOf(oldFlags);
+            let newDesc;
+            if (oldFlagsIndex === -1) {
+                newDesc = oldDesc.trimEnd() + '\n\n' + getChannelFlags();
+            }
+            else {
+                const part1 = oldDesc.slice(0, oldFlagsIndex);
+                const part2 = oldDesc.slice(oldFlagsIndex + oldFlags.length);
+                newDesc = part1 + getChannelFlags() + part2;
+            }
+
+            authWindow.location.href = (`https://mixer.com?bettermixer-message=description&bettermixer-auth=${localStorage.getItem('mixerAuthToken')}&bettermixer-message-data=${btoa(newDesc)}`);
+            setTimeout(function closedCheck() {
+                if (authWindow.closed) {
+                    document.getElementById('updated-at').innerText = "Updated at " + new Date();
+                }
+                else {
+                    setTimeout(closedCheck, 100);
+                }
+            }, 100);
+        }
+        catch (e) {
+            if (e.status === 401) {
+                localStorage.removeItem('mixerAuthToken');
+                getAuthToken(authWindow);
+            }
+            else {
+                document.getElementById('updated-at').innerText = "Failed to update automatically. Either authorization was denied or something else broke...";
+                console.log(e);
+                authWindow.close();
+            }
+        }
+    }
+    else {
+        getAuthToken(authWindow);
+    }
+}
+
+const getAuthToken = (authWindow) => {
+    const dirPath = /.*?(?:\/.*?)*(?=\/)(?:(?!.*\.)\/.+(?!\/$))?/.exec(window.location.href);
+    const authUrl = `https://mixer.com/oauth/authorize?client_id=473772fa4fa6d46b0c73f2aca560215cf602d1c2e925e521&response_type=token&redirect_uri=${dirPath}/oauth_redirect.html&scope=channel:update:self`;
+    
+    authWindow.location.replace(authUrl);
+
+    const oauthReturnCheck = e => {
+        try { authWindow.location.hostname; } catch {
+            setTimeout(oauthReturnCheck, 100);
+            return;
+        }
+        let sp;
+        if (authWindow.location.search == '') {
+            sp = new URLSearchParams(authWindow.location.hash.replace('#', '?'))
+        }
+        else {
+            sp = new URLSearchParams(authWindow.location.search);
+        }
+        const token = sp.get('access_token');
+        if (token !== '') {
+            localStorage.setItem('mixerAuthToken', token);
+            updateMixerDescription(authWindow);
+        }
+        else {
+            document.getElementById('updated-at').innerText = "Failed to update automatically. Either authorization was denied or something else broke...";
+        }
+    }
+    setTimeout(oauthReturnCheck, 100);
 }
